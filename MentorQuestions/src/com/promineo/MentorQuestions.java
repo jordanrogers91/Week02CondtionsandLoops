12942b91e0a196afac3138abde493e8bf72a9978
Jordan Rogers <jordanrogers@Jordans-MacBook-Air.local>
2023-12-01 12:22:18 -0700

    added new java files

---
+++ MentorQuestions/src/com/promineo/MentorQuestions.java
@@ -0,0 +1,11 @@
+package com.promineo;
+
+public class MentorQuestions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	/*Complete the method called passwordChecker that accepts the String stored in variable password. The method should check to make sure that password is at least 8 characters in length and contains one of 4 special symbols, !, $, %, or &. The method should return true or false.*/
+	
+}
